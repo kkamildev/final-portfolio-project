fe9fae251fb3e4381bcf7d3ced6e35f59d6d5285
kkamildev <kamil.kijak@pixlesofte.com>
2026-04-30 23:03:56 +0200

    almost finished HistoryPage

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -28,7 +28,7 @@ const HistoryPage : FC<Props> = ({}) => {
             />
             <section className="m-2 p-2 flex flex-col text-white">
                 <PageTitle title="My history" subtitle="My interesting learning and working path"/>
-                <section className="">
+                <section className="my-10">
                     <h1 className="text-3xl font-extrabold">My learning Path to greatness</h1>
                     <section className="h-2.5 bg-yellow-500 my-5 rounded-2xl w-1/2"></section>
                     <p className="font-bold text-zinc-600 mt-2"></p>
@@ -37,11 +37,11 @@ const HistoryPage : FC<Props> = ({}) => {
                     <aside className="flex-1 flex flex-col items-center">
                         <section className="flex flex-col items-center relative">
                             <section className="h-16 w-16 rounded-full bg-yellow-500 relative top-2 flex justify-center items-center">
-                                <section className="h-8 w-8 rounded-full bg-zinc-900 relative"></section>
+                                <section className="h-8 w-8 rounded-full bg-zinc-900 relative animate-pulse"></section>
                             </section>
                             <section className="h-150.5 lg:h-100.5 w-5 bg-yellow-500"></section>
                             <section className="h-16 w-16 rounded-full bg-orange-500 relative bottom-2 flex justify-center items-center z-20">
-                                <section className="h-8 w-8 rounded-full bg-zinc-900 relative"></section>
+                                <section className="h-8 w-8 rounded-full bg-zinc-900 relative animate-pulse"></section>
                             </section>
                         </section>
                         <section className="flex flex-col items-center relative bottom-4">
@@ -75,6 +75,46 @@ const HistoryPage : FC<Props> = ({}) => {
                             <p className="font-bold text-zinc-600 mt-2">
                                 I want go to college on <span className="text-orange-500">Computer Science field</span>, I want develop my potential in IT and also<span className="text-orange-500"> in Math (I love Math)</span>. I guess I know so much thinks but
                                 I want knowing more to be a specialist in this branch, to develop my greatness path. I hope everyting will be okay and I will be improving at college
+                            </p>
+                        </section>
+                    </aside>
+                </section>
+                <section className="my-10">
+                    <h1 className="text-3xl font-extrabold">My working path to success</h1>
+                    <section className="h-2.5 bg-green-700 my-5 rounded-2xl w-1/2"></section>
+                    <p className="font-bold text-zinc-600 mt-2"></p>
+                </section>
+                <section className="w-full flex mt-10">
+                    <aside className="flex-1 flex flex-col items-center">
+                        <section className="flex flex-col items-center relative">
+                            <section className="h-16 w-16 rounded-full bg-green-700 relative top-2 flex justify-center items-center">
+                                <section className="h-8 w-8 rounded-full bg-zinc-900 relative animate-pulse"></section>
+                            </section>
+                            <section className="h-150.5 lg:h-100.5 w-5 bg-green-700 "></section>
+                            <section className="h-16 w-16 rounded-full bg-blue-800 relative bottom-2 flex justify-center items-center z-20">
+                                <section className="h-8 w-8 rounded-full bg-zinc-900 relative animate-pulse"></section>
+                            </section>
+                        </section>
+                        <section className="flex flex-col items-center relative bottom-4">
+                            <section className="h-120.5 lg:h-80.5 w-5 bg-blue-800"></section>
+                        </section>
+                    </aside>
+                    <aside className="flex-2 flex flex-col items-center">
+                        <section className="flex-2 mt-10">
+                            <h3 className="text-2xl font-extrabold">SKINVEST Specific-task contract</h3>
+                            <section className="h-2.5 bg-green-700 my-5 rounded-2xl w-1/2"></section>
+                            <h4 className="text-2xl font-extrabold text-zinc-600">Making web full-stack property management system June - July 2025</h4>
+                            <p className="font-bold text-zinc-600 mt-2">
+                                It was my first form of job, I had to create a <span className="text-green-700">full-stack system</span> to replace excel database because this was messy, so system was a good option to create <br />
+                                I used <span className="text-green-700">node.js with express, react.js, MySQL as database and tailwind for styles</span> to create it. This job have teached me new technologies and how to deploy a production versions
+                            </p>
+                        </section>
+                        <section className="flex-2 mt-10">
+                            <h3 className="text-2xl font-extrabold">Looking for intern</h3>
+                            <section className="h-2.5 bg-blue-800 my-5 rounded-2xl w-1/2"></section>
+                            <h4 className="text-2xl font-extrabold text-zinc-600">Looking for software-developer intern</h4>
+                            <p className="font-bold text-zinc-600 mt-2">
+                                I have been looking for <span className="text-blue-800">software-developer intern</span> to improve my skills, career and get important experience in this work field. Everyone must start from zero, so I really want to make the first step
                             </p>
                         </section>
                     </aside>
